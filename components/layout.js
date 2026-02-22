96cba51742fb91088032f266a59a160d107141c1
henryyeoj <hysdelacruz.student@ua.edu.ph>
2026-02-22 14:49:39 +0800

    Remove comment about updating name in layout.js

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Henry Yeoj Dela Cruz'; // Updated to your name
+const name = 'Henry Yeoj Dela Cruz'; 
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
